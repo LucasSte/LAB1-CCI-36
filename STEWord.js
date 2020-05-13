84a402247e4d831d7a4f9d2cbc4743099287fa04
Lucas <lucas.tnagel@gmail.com>
2020-05-13 17:28:37 -0400

    Making letters rotate

---
+++ STEWord.js
@@ -0,0 +1,90 @@
+import * as THREE from "./three.js/build/three.module.js";
+
+class STEWord
+{
+    horizontalBar = new THREE.BoxGeometry(4, 1, 1);
+    verticalBar = new THREE.BoxGeometry(1, 2, 1);
+    materialSte = new THREE.MeshLambertMaterial({color: 0xff0000});
+
+    S1 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+    S2 = new THREE.Mesh(this.verticalBar, this.materialSte);
+    S3 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+    S4 = new THREE.Mesh(this.verticalBar, this.materialSte);
+    S5 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+
+    tBarGeometry = new THREE.BoxGeometry(1, 6, 1);
+    T1 = new THREE.Mesh(this.tBarGeometry, this.materialSte);
+    T2 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+
+    E1 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+    E2 = new THREE.Mesh(this.verticalBar, this.materialSte);
+    E3 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+    E4 = new THREE.Mesh(this.verticalBar, this.materialSte);
+    E5 = new THREE.Mesh(this.horizontalBar, this.materialSte);
+
+    baseOneGeometry = new THREE.BoxGeometry(18, 1, 3);
+    baseTwoGeometry = new THREE.BoxGeometry(20, 1, 5);
+    baseMaterial = new THREE.MeshLambertMaterial({color: 0x6d6d6d});
+
+    baseOne = new THREE.Mesh(this.baseOneGeometry, this.baseMaterial);
+    baseTwo = new THREE.Mesh(this.baseTwoGeometry, this.baseMaterial);
+
+    SGroup = new THREE.Group();
+    TGroup = new THREE.Group();
+    EGroup = new THREE.Group();
+    STEGroup = new THREE.Group();
+    baseGroup = new THREE.Group();
+    steAndBase = new THREE.Group();
+
+    constructor() {
+        this.S1.position.set(0, 0.5, 0);
+        this.S2.position.set(1.5, 2, 0);
+        this.S3.position.set(0, 3.5, 0);
+        this.S4.position.set(-1.5, 5, 0);
+        this.S5.position.set(0, 6.5, 0);
+
+        this.T1.position.set(0, 3, 0);
+        this.T2.position.set(0, 6.5, 0);
+
+        this.E1.position.set(0, 0.5, 0);
+        this.E2.position.set(-1.5, 2, 0);
+        this.E3.position.set(0, 3.5, 0);
+        this.E4.position.set(-1.5, 5, 0);
+        this.E5.position.set(0, 6.5, 0);
+
+        this.baseOne.position.y = 1.5;
+        this.baseTwo.position.y = 0.5;
+    }
+
+     buildGroups() {
+        this.SGroup.add(this.S1);
+        this.SGroup.add(this.S2);
+        this.SGroup.add(this.S3);
+        this.SGroup.add(this.S4);
+        this.SGroup.add(this.S5);
+        this.SGroup.position.x = -5;
+
+        this.TGroup.add(this.T1);
+        this.TGroup.add(this.T2);
+
+        this.EGroup.add(this.E1);
+        this.EGroup.add(this.E2);
+        this.EGroup.add(this.E3);
+        this.EGroup.add(this.E4);
+        this.EGroup.add(this.E5);
+        this.EGroup.position.x = 5;
+
+        this.STEGroup.add(this.SGroup);
+        this.STEGroup.add(this.TGroup);
+        this.STEGroup.add(this.EGroup);
+        this.STEGroup.position.y = 2;
+
+        //this.baseGroup.add(this.baseOne);
+        //this.baseGroup.add(this.baseTwo);
+
+        this.steAndBase.add(this.STEGroup);
+        this.steAndBase.add(this.baseOne);
+    }
+}
+
+export default STEWord;
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import * as THREE from "./three.js/build/three.module.js";
 import {OrbitControls} from "./three.js/examples/jsm/controls/OrbitControls.js";
-
+import STEWord from "./STEWord.js";
+//import rotateAroundAxis from "./axisRotate.js";
 
 let scene = new THREE.Scene();
 scene.color = new THREE.Color(89, 73, 40);
 scene.fog = new THREE.Fog(0x594928, 0.1, 80);
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+let renderer = new THREE.WebGLRenderer({canvas: document.getElementById("canv")});
 renderer.setClearColor(0x594928, 1);
 document.body.appendChild(renderer.domElement);
 
@@ -19,94 +19,11 @@ plane.rotateX(- Math.PI / 2);
 
 scene.add(plane);
 
-let horizontalBar = new THREE.BoxGeometry(4, 1, 1);
-let verticalBar = new THREE.BoxGeometry(1, 2, 1);
-let materialSte = new THREE.MeshLambertMaterial({color: 0xff0000});
+let word = new STEWord();
+word.buildGroups();
 
-
-let S1 = new THREE.Mesh(horizontalBar, materialSte);
-S1.position.set(0, 0.5, 0);
-
-let S2 = new THREE.Mesh(verticalBar, materialSte);
-S2.position.set(1.5, 2, 0);
-
-let S3 = new THREE.Mesh(horizontalBar, materialSte);
-S3.position.set(0, 3.5, 0);
-
-let S4 = new THREE.Mesh(verticalBar, materialSte);
-S4.position.set(-1.5, 5, 0);
-
-let S5 = new THREE.Mesh(horizontalBar, materialSte);
-S5.position.set(0, 6.5, 0);
-
-var SGroup = new THREE.Group();
-SGroup.add(S1);
-SGroup.add(S2);
-SGroup.add(S3);
-SGroup.add(S4);
-SGroup.add(S5);
-SGroup.position.x = -5;
-
-let tBarGeometry = new THREE.BoxGeometry(1, 6, 1);
-let T1 = new THREE.Mesh(tBarGeometry, materialSte);
-T1.position.set(0, 3, 0);
-
-let T2 = new THREE.Mesh(horizontalBar, materialSte);
-T2.position.set(0, 6.5, 0);
-
-var TGroup = new THREE.Group();
-TGroup.add(T2);
-TGroup.add(T1);
-
-let E1 = new THREE.Mesh(horizontalBar, materialSte);
-E1.position.set(0, 0.5, 0);
-
-let E2 = new THREE.Mesh(verticalBar, materialSte);
-E2.position.set(-1.5, 2, 0);
-
-let E3 = new THREE.Mesh(horizontalBar, materialSte);
-E3.position.set(0, 3.5, 0);
-
-let E4 = new THREE.Mesh(verticalBar, materialSte);
-E4.position.set(-1.5, 5, 0);
-
-let E5 = new THREE.Mesh(horizontalBar, materialSte);
-E5.position.set(0, 6.5, 0);
-
-let EGroup = new THREE.Group();
-EGroup.add(E1);
-EGroup.add(E2);
-EGroup.add(E3);
-EGroup.add(E4);
-EGroup.add(E5);
-EGroup.position.x = 5;
-
-var STEGroup = new THREE.Group();
-STEGroup.add(SGroup);
-STEGroup.add(TGroup);
-STEGroup.add(EGroup);
-
-let baseOneGeometry = new THREE.BoxGeometry(18, 1, 3);
-let baseTwoGeometry = new THREE.BoxGeometry(20, 1, 5);
-let baseMaterial = new THREE.MeshLambertMaterial({color: 0x6d6d6d});
-
-let baseOne = new THREE.Mesh(baseOneGeometry, baseMaterial);
-let baseTwo = new THREE.Mesh(baseTwoGeometry, baseMaterial);
-baseOne.position.y = 1.5;
-baseTwo.position.y = 0.5;
-
-var baseGroup = new THREE.Group();
-baseGroup.add(baseOne);
-baseGroup.add(baseTwo);
-
-STEGroup.position.y = 2;
-
-var steAndBase = new THREE.Group();
-steAndBase.add(baseGroup);
-steAndBase.add(STEGroup);
-
-scene.add(steAndBase);
-
+scene.add(word.steAndBase);
+scene.add(word.baseTwo);
 
 let light = new THREE.PointLight(0xffffff);
 light.position.set(0, 20, 10);
@@ -115,9 +32,116 @@ scene.add(light);
 var lightAngle = 0;
 
 camera.position.y = 10;
+camera.position.x = 5;
 let controls = new OrbitControls(camera, renderer.domElement);
+var fixate;
+var baseRotation;
 
+//Teste
+//word.EGroup.rotateY(-Math.PI /2);
+
+let sphereGeometry = new THREE.SphereGeometry(1);
+let sphereMaterial = new THREE.MeshBasicMaterial({color: 0x6d6d6d});
+let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+sphere.position.set(15, 1, 0);
+
+scene.add(sphere);
+
+function nextPos(x)
+{
+    return 17/2 - 30/289*Math.pow(x - 13/2, 2);
+}
+
+var num = 0;
+var down;
+var stage = 0;
+var angleSum = 500;
+var sum = 0;
+
+let ePoint = new THREE.Vector3(4.5, 0,0);
+let tPoint = new THREE.Vector3(-0.5, 0, 0);
+var eAxis = new THREE.Vector3(0,0,1);
+var q = new THREE.Quaternion();
 let animate = function () {
+    num = num + 1;
+    fixate = document.getElementById("fixate").checked;
+    down = document.getElementById("down").checked;
+    baseRotation = parseFloat(document.getElementById("vBase").value)*0.2*Math.PI/180;
+    if(fixate && !down)
+    {
+        word.SGroup.rotateY(-baseRotation);
+        word.EGroup.rotateY(-baseRotation);
+        word.TGroup.rotateY(-baseRotation);
+        word.steAndBase.rotateY(baseRotation);
+    }
+    else if(!down && !fixate)
+    {
+        word.EGroup.rotateY(parseFloat(document.getElementById("vE").value)*0.2*Math.PI/180);
+        word.SGroup.rotateY(parseFloat(document.getElementById("vS").value)*0.2*Math.PI/180);
+        word.TGroup.rotateY(parseFloat(document.getElementById("vT").value)*0.2*Math.PI/180);
+        word.steAndBase.rotateY(baseRotation);
+    }
+    else if(down && !fixate)
+    {
+        if(stage === 0)
+        {
+            word.steAndBase.rotation.y = 0;
+            word.EGroup.rotation.y = Math.PI / 2;
+            word.SGroup.rotation.y = Math.PI / 2;
+            word.TGroup.rotation.y = Math.PI / 2;
+            stage = 1;
+            num = 0;
+        }
+        else if(stage === 1)
+        {
+            sphere.position.x = 15 - num*0.3;
+            sphere.position.y = nextPos(sphere.position.x);
+            if(sphere.position.x <= 6.5)
+            {
+                stage = 2;
+                num = 0;
+            }
+        }
+        else if (stage === 2)
+        {
+            if(sphere.position.x <= 15)
+            {
+                sphere.position.x = 6.5 + num*0.2;
+                sphere.position.y = nextPos(sphere.position.x);
+            }
+            if(sum <= 0.608)
+            {
+                angleSum -= 10;
+                q.setFromAxisAngle(eAxis, Math.PI/angleSum);
+                sum += Math.PI /angleSum;
+                word.EGroup.applyQuaternion(q);
+                word.EGroup.position.sub(ePoint);
+                word.EGroup.position.applyQuaternion(q);
+                word.EGroup.position.add(ePoint);
+            }
+            else
+            {
+                q.setFromAxisAngle(eAxis, Math.PI / angleSum);
+                sum += Math.PI /angleSum;
+                word.EGroup.applyQuaternion(q);
+                word.EGroup.position.sub(ePoint);
+                word.EGroup.position.applyQuaternion(q);
+                word.EGroup.position.add(ePoint);
+
+                word.TGroup.applyQuaternion(q);
+                word.TGroup.position.sub(tPoint);
+                word.TGroup.position.applyQuaternion(q);
+                word.TGroup.position.add(tPoint);
+            }
+            //sphere.position.set(4.5, 2, 0);
+
+        }
+
+
+    }
+    //sphere.position.x = 15 - num*0.01;
+    //sphere.position.y = nextPos(sphere.position.x);
 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
